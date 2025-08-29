bc6b2d880856dd928e74c4f2b79d9443bb9d3d2b
Muhammad Younis <binnazar7575@gmail.com>
2025-08-29 04:49:53 -0700

    updated

---
+++ lib/screens/splash/splash_controller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> scaleAnimation;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    scaleAnimation = Tween<double>(begin: 0.6, end: 1.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOutBack,
+      ),
+    );
+
+    fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    animationController.forward();
+
+    // Navigate to login screen after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      // Get.offAll(() => const LoginScreen());
+    });
+  }
+
+  @override
+  void onClose() {
+    animationController.dispose();
+    super.onClose();
+  }
+}
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,54 @@
+import 'package:bmw_passes/screens/splash/splash_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Colors.amber));
+    final controller = Get.put(SplashController());
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: controller.animationController,
+          builder: (context, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Transform.scale(
+                  scale: controller.scaleAnimation.value,
+                  child: Opacity(
+                    opacity: controller.fadeAnimation.value,
+                    child: Image.asset(
+                      "assets/images/appicon.png", // your app icon
+                      width: Get.width * 0.3,
+                      height: Get.width * 0.3,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Opacity(
+                  opacity: controller.fadeAnimation.value,
+                  child: Text(
+                    "BMW M", // your app name
+                    style: TextStyle(
+                      fontSize: Get.width * 0.07,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
+
