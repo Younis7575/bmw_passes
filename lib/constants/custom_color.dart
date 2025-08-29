cacc5631459d7e9543479487aabdd15e35e68fce
Muhammad Hamza <muhammadhamzametla@gmail.com>
2025-08-29 16:29:12 +0500

    upadate

---
+++ lib/constants/custom_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color screenBackground = Color(0xffFFFFFF);
+  static const Color mainText = Color(0xff0066B1);
+  static const Color contentText = Color(0xff9098B1);
+  static const Color emailpasswors = Color(0xff6F6F6F);
+  static const Color slider = Color(0xffD9D9D9);
+  static const Color dot = Color(0xffE22718);
+}
